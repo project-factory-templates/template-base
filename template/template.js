ba9d6512a2cf5ae4d64b543da2ac5aad217b58f8
retueZe <84510709+retueZe@users.noreply.github.com>
2023-06-12 17:38:00 +0300

    r0

---
+++ template/template.js
@@ -0,0 +1,13 @@
+export default {
+    promptScript: [
+        {
+            name: 'AUTHOR',
+            type: 'text',
+            message: 'Author:'
+        }
+    ],
+    onPromptSubmit: variables => ({
+        ...variables,
+        YEAR: new Date().getFullYear()
+    })
+}
