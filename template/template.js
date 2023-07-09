7cb05b290735e0956ed3fbc194033d2432c412a1
retueZe <84510709+retueZe@users.noreply.github.com>
2023-07-09 09:05:12 +0300

    Add Git support

--- template/template.js
+++ template/template.js
@@ -1,12 +1,32 @@
+import { $ } from 'execa'
+
 export default {
     promptScript: [
         {
             name: 'AUTHOR',
             type: 'text',
             message: 'Author:'
+        },
+        {
+            name: 'INIT_GIT',
+            type: 'confirm',
+            message: 'Would you like to initialize Git automatically?',
+            initial: true
         }
     ],
     onPromptSubmit: variables => {
         variables.YEAR = new Date().getFullYear()
+    },
+    onScaffolded: async (dir, vars) => {
+        if (!vars.INIT_GIT) return
+
+        const $$ = $({
+            cwd: dir,
+            stderr: 'inherit'
+        })
+
+        await $$`git init`
+        await $$`git add .`
+        await $$`git commit -m ${'Initial commit'}`
     }
 }
